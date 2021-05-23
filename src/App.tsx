f459b40149452bd6c13850214c105803bb6c012a
Matthías Sigurbjörnsson <matthias.sigurbjornsson@advania.is>
2021-05-23 20:04:04 +0000

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { SocketClient } from "./lib/socketClient";
 import { SocketClientProvider } from "./lib/socketClient";
 import { UserForm } from "./components/UserNameForm";
 
+// making sure socket client is initialized once
 const socketClient = new SocketClient();
 
 export const App: React.FunctionComponent = function () {
